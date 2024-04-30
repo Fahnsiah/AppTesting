2727fbdedaf31b6e643740c942a5ec378f651321
Henry <hfwilson@mwetana.com.lr>
2024-04-30 19:58:32 +0000

    first codes for app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Counter from './components/counter/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Home Page</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+
+
